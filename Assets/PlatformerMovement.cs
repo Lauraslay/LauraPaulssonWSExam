1bce9b6a503a478a9a39975a32f31f1aa9764295
Lauraslay <a25laupa@student.his.se>
2025-11-21 15:50:20 +0100

    animations, sfx

--- Assets/PlatformerMovement.cs
+++ Assets/PlatformerMovement.cs
@@ -94,6 +94,7 @@ public class PlatformerMovement : MonoBehaviour
         rb.linearVelocity = velocity;
         
         // Write movement animation code here. (Suggestion: send your current velocity into the Animator for both the x- and y-axis.)
+        animator.SetFloat("speed", Mathf.Abs(velocity.x));
     }
 
     private bool IsGrounded()
